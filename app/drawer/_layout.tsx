6c80d8cff5457a59f11e22f5a0262af78ace6e74
AlexanderJumbo <geossephyjumboalava@gmail.com>
2025-10-05 12:08:12 -0500

    Personalización del drawer

--- app/drawer/_layout.tsx
+++ app/drawer/_layout.tsx
@@ -1,15 +1,28 @@
 import { View, Text } from "react-native";
 import React from "react";
 import Drawer from "expo-router/drawer";
+import { Ionicons } from "@expo/vector-icons";
 
 const DrawerLayout = () => {
   return (
-    <Drawer>
+    <Drawer
+      screenOptions={{
+        overlayColor: "rgba(0,0,0,0.4)",
+        drawerActiveTintColor: "indigo",
+        headerShadowVisible: false,
+        sceneStyle: {
+          backgroundColor: "white",
+        },
+      }}
+    >
       <Drawer.Screen
         name="user/index" // This is the name of the page and must match the url from root
         options={{
           drawerLabel: "User",
           title: "Usuario",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="person-circle-outline" size={size} color={color} />
+          ),
         }}
       />
       <Drawer.Screen
@@ -17,6 +30,9 @@ const DrawerLayout = () => {
         options={{
           drawerLabel: "Horarios",
           title: "Horarios",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="calendar-outline" size={size} color={color} />
+          ),
         }}
       />
     </Drawer>
